e772b410159142d3450a3643a71e40b36e7d690c
LightVillet <maxim12000@ya.ru>
2021-04-25 20:04:55 +0300

    Edit for downloading Kormen

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-from flask import Flask, render_template
+from os import path
+import json
+
+from flask import Flask, render_template, request, send_from_directory
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
-    return render_template("index.html")
+    return send_from_directory(app.root.path, 'Kormen-T-Algoritmy-postroenie-i-analiz-3-e-izdanie.djvu')
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(port='1337', debug=True)
 
